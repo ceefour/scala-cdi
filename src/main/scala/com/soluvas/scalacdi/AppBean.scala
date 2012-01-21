99f7381f1d451b7765120e309f7050a904d0eb74
Hendy Irawan <hendy@soluvas.com>
2012-01-22 02:58:58 +0700

    Infinispan CDI activated, still cannot reproduce

--- src/main/scala/com/soluvas/scalacdi/AppBean.scala
+++ src/main/scala/com/soluvas/scalacdi/AppBean.scala
@@ -1,12 +1,12 @@
 package com.soluvas.scalacdi
 import scala.collection.JavaConversions.seqAsJavaList
-
 import org.slf4j.LoggerFactory
-
 import javax.annotation.PostConstruct
 import javax.cache.annotation.CacheResult
 import javax.enterprise.context.ApplicationScoped
 import javax.inject.Named
+import javax.servlet.ServletContext
+import javax.enterprise.event.Observes
 
 /**
  * Testing @ApplicationScoped bean.
@@ -18,12 +18,19 @@ class AppBean {
   private val _car = "Toyota"
   private val _animals = List("Bird", "Mammoth", "Elephant", "Giraffe", "Tiger")
   
+  def servletStarted(@Observes context: ServletContext) = {
+    log.info("Servlet {} started", context.getContextPath())
+  }
+  
   @PostConstruct def init(): Unit = {
     log.info("Started {}", getClass())
   }
   
   def getCar() = _car
   
-  @CacheResult def getAnimals(): java.util.List[String] = _animals 
+  @CacheResult def getAnimals(): java.util.List[String] = {
+    log.info("Loading animals")
+    _animals
+  }
 
 }
