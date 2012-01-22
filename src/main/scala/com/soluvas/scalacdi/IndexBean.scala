e81388484e00762738c3d19862afe8a1de59e629
Hendy Irawan <hendy@soluvas.com>
2012-01-22 16:20:25 +0700

    Bug is reproducible! Along with workaround! :-)

--- src/main/scala/com/soluvas/scalacdi/IndexBean.scala
+++ src/main/scala/com/soluvas/scalacdi/IndexBean.scala
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory
 import javax.annotation.PostConstruct
 import scala.reflect.BeanProperty
 import javax.cache.annotation.CacheResult
+import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 /**
  * Testing @RequestScoped bean.
@@ -12,7 +15,7 @@ import javax.cache.annotation.CacheResult
 @RequestScoped @Named
 class IndexBean {
   
-  private val log = LoggerFactory.getLogger(classOf[IndexBean])
+  private lazy val log = LoggerFactory.getLogger(classOf[IndexBean])
   @BeanProperty var city = "New York"
   
   @PostConstruct def init(): Unit = {
@@ -20,5 +23,22 @@ class IndexBean {
   }
   
   @CacheResult def getAddress() = "5015 Elm Street"
+
+  def getKind(gender: String) = {
+    List("a", "b", "c").foreach { println(_) }
+    gender match {
+      case "male" => "Rock solid"
+      case "female" => "Warm and soft"
+    }
+  }
+  
+  def testExecutor() = {
+    val executor = Executors.newFixedThreadPool(4);
+    // this avoids 'log' becoming 'final' like:
+    //     private org.slf4j.Logger com$soluvas$scalacdi$IndexBean$$log;
+    //     public final org.slf4j.Logger com$soluvas$scalacdi$IndexBean$$log();
+    val log = this.log; 
+    executor.submit(new Runnable() { override def run(): Unit = log.debug("Executor is running") })
+  }
 
 }
